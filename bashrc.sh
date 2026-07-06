40a499748fd7d266cf4775f7e2f28630af1cd028
Andrew-v-C <andrewvancampen@gmail.com>
2026-07-06 13:07:26 -0400

    Removed an alias

--- bashrc.sh
+++ bashrc.sh
@@ -4,8 +4,7 @@
 source /etc/skel/.bashrc
 
 # Create aliases
-alias ar="arduino-cli"
 alias dl="wget -P ~/Downloads"
 alias ed="nvim"
 alias py="python3"
-alias update="sudo apt update -y; sudo apt upgrade -y; sudo apt autoremove -y; sudo apt autoclean -y"
+# alias update="sudo apt update -y; sudo apt upgrade -y; sudo apt autoremove -y; sudo apt autoclean -y"
