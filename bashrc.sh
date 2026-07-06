43841638d160bdbb8c4b36fd98bd833ecefc1381
Andrew-v-C <andrewvancampen@gmail.com>
2026-07-06 12:13:30 -0400

    Added new alias for downloads

--- bashrc.sh
+++ bashrc.sh
@@ -5,6 +5,7 @@ source /etc/skel/.bashrc
 
 # Create aliases
 alias ar="arduino-cli"
+alias dl="wget -P ~/Downloads"
 alias ed="nvim"
 alias py="python3"
-alias update="sudo apt update; sudo apt upgrade; sudo apt autoremove; sudo apt autoclean"
+alias update="sudo apt update -y; sudo apt upgrade -y; sudo apt autoremove -y; sudo apt autoclean -y"
